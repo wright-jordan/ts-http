e776d12a72c882e96106beaada8f70fe2f02b9c3
Jordan Wright <wright.jordan.n@outlook.com>
2022-02-01 01:39:11 -0600

    use type instead of interface and add middleware type

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,17 @@
 import http, { IncomingMessage, ServerResponse } from "http";
 
-export interface AsyncRequestListener {
-  (req: IncomingMessage, res: ServerResponse): Promise<void>;
-}
+export type AsyncRequestListener = (
+  req: IncomingMessage,
+  res: ServerResponse
+) => Promise<void>;
 
 export type Handlers = {
   [path: string]: AsyncRequestListener;
 };
+
+export type MiddleWare = (
+  next: AsyncRequestListener
+) => Promise<AsyncRequestListener>;
 
 export default function Mux(
   handlers: Handlers,
