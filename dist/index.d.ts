202b0b9cfb1e927639e1a6d965fc7a731e879139
Jordan Wright <wright.jordan.n@outlook.com>
2022-02-01 01:43:56 -0600

    compile

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -4,7 +4,7 @@ export declare type AsyncRequestListener = (req: IncomingMessage, res: ServerRes
 export declare type Handlers = {
     [path: string]: AsyncRequestListener;
 };
-export declare type MiddleWare = (next: AsyncRequestListener) => Promise<AsyncRequestListener>;
+export declare type Middleware = (next: AsyncRequestListener) => Promise<AsyncRequestListener>;
 export default function Mux(handlers: Handlers, _404: AsyncRequestListener): AsyncRequestListener;
 export declare function readBuf(req: IncomingMessage, options?: {
     maxBytes: number;
