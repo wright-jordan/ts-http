5fe5a86f7d44fc40612d8a3b61242de4c4494c94
Jordan Wright <wright.jordan.n@outlook.com>
2022-02-06 13:27:41 -0600

    compile

--- dist/index.js
+++ dist/index.js
@@ -1,8 +1,8 @@
 import http from "http";
 export function App(next) {
-    return async function (r, w) {
+    return function (r, w) {
         const ctx = {};
-        await next(r, w, ctx);
+        next(r, w, ctx);
     };
 }
 export function Mux(handlers, _404) {
